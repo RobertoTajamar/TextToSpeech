144ff72fb632ecec86eda083c5e39038320ed37c
RobertoTajamar <37329405+RobertoTajamar@users.noreply.github.com>
2018-06-06 13:07:08 +0200

    Add files via upload

---
+++ TextSpeech.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Plugin.TextToSpeech;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Testing
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    //(http://)tiendaproductospgs.azurewebsites.net/odata/productos 
+	public partial class TextSpeech : ContentPage
+	{
+		public TextSpeech ()
+		{
+			InitializeComponent ();
+            this.btnHablar.Clicked += BtnHablar_Clicked;
+		}
+
+        private void BtnHablar_Clicked(object sender, EventArgs e)
+        {
+            CrossTextToSpeech.Current.Speak(this.txtSpeech.Text);
+        }
+    }
+}
